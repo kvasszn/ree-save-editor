bbc17057f726915b473577c6725b79daab3b194c
kvasszn <kvasszn@gmail.com>
2025-07-06 05:37:36 -0400

    Remove target"

--- rsz_macros/target/debug/build/proc-macro2-3de9c5a4c0283c20/build_script_build-3de9c5a4c0283c20.d
+++
@@ -1,5 +0,0 @@
-/home/nikola/programming/mhtame/rsz_macros/target/debug/build/proc-macro2-3de9c5a4c0283c20/build_script_build-3de9c5a4c0283c20.d: /home/nikola/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
-
-/home/nikola/programming/mhtame/rsz_macros/target/debug/build/proc-macro2-3de9c5a4c0283c20/build_script_build-3de9c5a4c0283c20: /home/nikola/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
-
-/home/nikola/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs:
--- rsz_macros/target/debug/deps/unicode_ident-d65c8a87cca24891.d
+++
@@ -1,6 +0,0 @@
-/home/nikola/programming/mhtame/rsz_macros/target/debug/deps/unicode_ident-d65c8a87cca24891.d: /home/nikola/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /home/nikola/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/home/nikola/programming/mhtame/rsz_macros/target/debug/deps/libunicode_ident-d65c8a87cca24891.rmeta: /home/nikola/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /home/nikola/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/home/nikola/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs:
-/home/nikola/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs:
